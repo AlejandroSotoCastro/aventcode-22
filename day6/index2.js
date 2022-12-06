337bf751b2407eb86e3a39cd5455d9e6da09fada
Alejandro Soto Castro <alejandrosc.eng@gmail.com>
2022-12-06 12:11:58 +0100

    Day6
    This was really easy

---
+++ day6/index2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const input = "./day6/inputs.txt";
+// const input = "./day6/inputsTest.txt";
+
+fs.readFile(input, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  for (let i = 0; i < data.length; i++) {
+    STP_MARKER = i + 14;
+    let marker = data.substring(i, STP_MARKER);
+    marker = marker.split("").filter((v, i) => marker.indexOf(v) === i);
+    if (marker.length === 14) {
+      console.log(STP_MARKER);
+      break;
+    }
+  }
+});
